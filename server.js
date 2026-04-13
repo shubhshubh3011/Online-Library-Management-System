2d6b79c3d8f939b9b35faddc064818a875195c50
Shubh Om <shubhom2004@gmail.com>
2026-04-13 14:54:39 +0530

    added styling

--- server.js
+++ server.js
@@ -1,23 +1,30 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
+const expressLayouts = require("express-ejs-layouts");
+require("dotenv").config();
 
 const app = express();
 
 // middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
-app.set("view engine", "ejs");
-app.use("/", require("./routes/userRoutes"));
 
-// DB connect
-mongoose.connect("mongodb+srv://shubhom2004_db_user:<password>@cluster0.burb1ya.mongodb.net/?appName=Cluster0")
+// view engine
+app.set("view engine", "ejs");
+
+// layouts
+app.use(expressLayouts);
+app.set("layout", "layout");
+
+// DB
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log("MongoDB Connected"))
 .catch(err => console.log(err));
 
 // routes
 app.use("/", require("./routes/bookRoutes"));
+app.use("/", require("./routes/userRoutes"));
 app.use("/", require("./routes/transactionRoutes"));
 
 app.listen(3000, () => {
